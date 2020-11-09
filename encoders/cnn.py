531af587a37c603297121ee2bb927173063a591b
Joe Dursun <joedursun@gmail.com>
2020-11-08 19:27:34 -0500

    Clean up cnn.py + workbook

--- encoders/cnn.py
+++ encoders/cnn.py
@@ -8,23 +8,34 @@ def conv3x3(in_channels=3, out_channels=3, stride=1):
         in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False
     )
 
-def calculate_conv_dims(h_w, kernel_size=3, stride=1, padding=0, dilation=1):
-    """
-    (H −1)×stride[0]−2×padding[0]+dilation[0]×(kernel_size[0]−1)+output_padding[0]+1
-W_{o
-    """
-    h_out = h_w[0]*stride - 2*padding + dilation * ( kernel_size - 1 ) + padding
-    w_out = h_w[1]*stride - 2*padding + dilation * ( kernel_size - 1 ) + padding
-    return (h_out, w_out)
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
 
-def calculate_pool_dims(h_w, kernel_size=3, stride=1, padding=0, dilation=1):
-    """
-    (H −1)×stride[0]−2×padding[0]+dilation[0]×(kernel_size[0]−1)+output_padding[0]+1
-W_{o
-    """
-    h_out = h_w[0]*stride - 2*padding + dilation * ( kernel_size - 1 ) + padding
-    w_out = h_w[1]*stride - 2*padding + dilation * ( kernel_size - 1 ) + padding
-    return (h_out, w_out)
+def get_layer(str):
+    name, attrs = str.split('(')
+    attrs = attrs.replace(')', '')
+    attrs = attrs.split(', ')
+    kwargs = {}
+    for attr in attrs:
+        if attr == '' or attr == None:
+            pass
+        else:
+            k,v = attr.split('=')
+            if v.isdigit():
+                v = int(v)
+            elif isfloat(v):
+                v = float(v)
+            elif v in ['True', 'False']:
+                v = bool(v)
+            kwargs[k] = v
+
+    mod = __import__('torch.nn', fromlist=[name])
+    klass = getattr(mod, name)
+    return klass(**kwargs)
 
 class ResidualBlock(torch.nn.Module):
     def __init__(self, num_channels, stride=1):
@@ -45,7 +56,7 @@ class ResidualBlock(torch.nn.Module):
         x += x
         x = torch.nn.functional.relu(x)
         return x
-    
+
 class AutoEncoder(torch.nn.Module):
     def __init__(
         self,
@@ -56,70 +67,84 @@ class AutoEncoder(torch.nn.Module):
     ):
         super().__init__()
         mid_channels = input_channels
-        mid_kernel_size = 6
+        mid_kernel_size = 8
 
-#         l1_dims = calculate_conv_dims(h_w, kernel_size=mid_kernel_size, stride=2, padding=2)
         self.conv1 = torch.nn.Sequential(
             torch.nn.Conv2d(input_channels, mid_channels, kernel_size=mid_kernel_size, stride=2, padding=2),
             torch.nn.LeakyReLU(),
-#             torch.nn.MaxPool2d(kernel_size=mid_kernel_size, stride=1, return_indices=True)
+            torch.nn.BatchNorm2d(mid_channels)
         )
 
-#         l2_dims = calculate_conv_dims(l1_dims, kernel_size=mid_kernel_size, stride=2, padding=2)
         self.conv2 = torch.nn.Sequential(
             torch.nn.Conv2d(mid_channels, mid_channels, kernel_size=mid_kernel_size, stride=2, padding=2),
             torch.nn.LeakyReLU(),
-#             torch.nn.MaxPool2d(kernel_size=mid_kernel_size, stride=1, return_indices=True)
+            torch.nn.BatchNorm2d(mid_channels)
         )
 
-#         l3_dims = calculate_conv_dims(l2_dims, kernel_size=mid_kernel_size, stride=2, padding=2)
         self.conv3 = torch.nn.Sequential(
             torch.nn.Conv2d(mid_channels, latent_channels, kernel_size=mid_kernel_size, stride=2, padding=2),
             torch.nn.LeakyReLU(),
-#             torch.nn.MaxPool2d(kernel_size=mid_kernel_size, stride=1, return_indices=True)
+            torch.nn.BatchNorm2d(mid_channels)
         )
-        
-        self.encoder = torch.nn.ModuleList([self.conv1, self.conv2, self.conv3])
-        
-#         self.unpool1 = torch.nn.MaxUnpool2d(kernel_size=mid_kernel_size, stride=1)
-        self.deconv1 = torch.nn.ConvTranspose2d(latent_channels, mid_channels, kernel_size=mid_kernel_size, stride=2, padding=2)
-        self.decode_activ1 = torch.nn.ReLU()
-        
-#         self.unpool2 = torch.nn.MaxUnpool2d(kernel_size=mid_kernel_size, stride=1)
-        self.deconv2 = torch.nn.ConvTranspose2d(mid_channels, mid_channels, kernel_size=mid_kernel_size, stride=2, padding=2)
-        self.decode_activ2 = torch.nn.ReLU()
-        
-#         self.unpool3 = torch.nn.MaxUnpool2d(kernel_size=mid_kernel_size, stride=1)
-        self.deconv3 = torch.nn.ConvTranspose2d(mid_channels, input_channels, kernel_size=mid_kernel_size, stride=2, padding=2)
-        self.decode_activ3 = torch.nn.ReLU()
-        self.batchnorm = torch.nn.BatchNorm2d(input_channels)
 
-#         self.decoder = torch.nn.ModuleList([self.deconv1, self.deconv2, self.deconv3])
-                
+        self.encoder = torch.nn.ModuleList([self.conv1, self.conv2, self.conv3])
+
+        self.deconv1 = torch.nn.Sequential(
+            torch.nn.ConvTranspose2d(latent_channels, mid_channels, kernel_size=mid_kernel_size, stride=2, padding=2),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(mid_channels)
+        )
+
+        self.deconv2 = torch.nn.Sequential(
+            torch.nn.ConvTranspose2d(mid_channels, mid_channels, kernel_size=mid_kernel_size, stride=2, padding=2),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm2d(mid_channels)
+        )
+
+        self.deconv3 = torch.nn.Sequential(
+            torch.nn.ConvTranspose2d(mid_channels, input_channels, kernel_size=mid_kernel_size, stride=2, padding=1),
+            torch.nn.Sigmoid(),
+        )
+
+        self.decoder = torch.nn.ModuleList([self.deconv1, self.deconv2, self.deconv3])
+
 #         self.resblocks = torch.nn.Sequential(*[ResidualBlock(output_channels) for _ in range(num_blocks)])
 
     def forward(self, x):
-#         pool_indices = []
         for layer in self.encoder:
             x = layer(x)
-#             print(x.shape, layer)
-#             pool_indices.append(indices)
 
         self.latent_vector = x
 
-#         pool_indices.reverse()
-#         x = self.unpool1(x, pool_indices[0])
-        x = self.deconv1(x)
-        x = self.decode_activ1(x)
-        
-#         print(x.shape, pool_indices[1].shape)
-#         x = self.unpool2(x, pool_indices[1])
-        x = self.deconv2(x)
-        x = self.decode_activ2(x)
-        x = self.batchnorm(x)
-        
-#         x = self.unpool3(x, pool_indices[2])
-        x = self.deconv3(x)
-        x = self.decode_activ3(x)
+        for layer in self.decoder:
+            x = layer(x)
+
         x = torch.clamp(x, max=255)
         return x
+
+    def save(self, filename, optimizer=None, loss=None, epoch=-1):
+        encoder_layers = [l.__repr__() for l in self.encoder.children()]
+        decoder_layers = [l.__repr__() for l in self.decoder.children()]
+        state = {
+            'encoder': encoder_layers,
+            'decoder': decoder_layers,
+            'model_state_dict': self.state_dict(),
+            'epoch': epoch,
+        }
+
+        if optimizer != None:
+            state['optimizer_state_dict'] = optimizer.state_dict()
+
+        if loss != None:
+            state['loss'] = loss.item()
+
+        torch.save(state, filename)
+
+    def load(self, filename, optimizer=None):
+        checkpoint = torch.load(filename)
+        self.encoder = nn.Sequential(*[get_layer(l) for l in checkpoint['encoder']])
+        self.decoder = nn.Sequential(*[get_layer(l) for l in checkpoint['decoder']])
+        self.load_state_dict(checkpoint['model_state_dict'])
+
+        if optimizer != None:
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
